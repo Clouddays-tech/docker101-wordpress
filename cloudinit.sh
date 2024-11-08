680b39534ed158be9e6a33bba98f07e14333e33f
Clouddays-tech <clouddays.info@gmail.com>
2024-11-08 17:14:39 +0800

    Update cloudinit.sh

--- cloudinit.sh
+++ cloudinit.sh
@@ -1,6 +1,5 @@
 sudo apt update && apt upgrade -y
 sudo curl -fsSL https://get.docker.com | sh
 sudo apt install docker-compose -y
-sudo groupadd docker 
-sudo usermod -aG docker $USER
+sudo groupadd docker && sudo usermod -aG docker $USER
 sudo systemctl restart docker
